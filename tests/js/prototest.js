9cec3f1ccbb7962676ee7eb35d8eeb4d0240d75b
Carlos Pardo <carlos.pardo@wunderman.com>
2017-04-12 10:10:32 -0300

    Finished the function light course

---
+++ tests/js/prototest.js
@@ -0,0 +1,26 @@
+
+
+console.log('in');
+
+var Animal = function (name) {
+	this.name = name;
+	this.eats = function () {
+		return this.name + " is eating."
+	}
+}
+
+/*
+Animal.prototype.eats = function () {
+	return this.name + " is eating."
+}
+*/
+
+Animal.prototype.age = 22;
+
+
+var perro = new Animal('jorge');
+
+//console.log(dog.__proto__.hasOwnProperty('toString'));
+
+
+console.log(perro);
